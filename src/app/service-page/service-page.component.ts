b2cc10048c69a4557da0a29bc1fcd5ca41b9cc32
Shivam Pandey <shivam.pandey@squarepixelstudios.net>
2022-04-15 17:41:28 +0530

    Service page card

--- src/app/service-page/service-page.component.ts
+++ src/app/service-page/service-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let tns: any;
 @Component({
   selector: 'app-service-page',
   templateUrl: './service-page.component.html',
-  styleUrls: ['./service-page.component.css']
+  styleUrls: ['./service-page.component.css'],
 })
 export class ServicePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    let slider = tns({
+      container: '.my-slider',
+      slideBy: 1,
+      // speed: 400,
+      nav: false,
+      controlsContainer: '#controls',
+      prevButton: '.previous',
+      nextButton: '.next',
+      responsive: {
+        1600: {
+          items: 4,
+          gutter: 20,
+        },
+        1024: {
+          items: 3,
+          gutter: 20,
+        },
+        768: {
+          items: 2,
+          gutter: 20,
+        },
+        480: {
+          items: 1,
+        },
+      },
+    });
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
