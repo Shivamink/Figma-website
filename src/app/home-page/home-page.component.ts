d842655f64b9b12cae4120e18f339ba13a69e7b7
Shivam Pandey <shivam.pandey@squarepixelstudios.net>
2022-04-15 14:48:24 +0530

    Home page + responsive & Card

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let tns: any;
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    let slider = tns({
+      container: '.my-slider',
+      slideBy: 1,
+      // speed: 400,
+      nav: false,
+      controlsContainer: '#controls',
+      prevButton: '.previous',
+      nextButton: '.next',
+      responsive: {
+        1600: {
+          items: 4,
+          gutter: 20,
+        },
+        1024: {
+          items: 3,
+          gutter: 20,
+        },
+        768: {
+          items: 2,
+          gutter: 20,
+        },
+        480: {
+          items: 1,
+        },
+      },
+    });
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
