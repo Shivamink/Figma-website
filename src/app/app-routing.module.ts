2bc11a3c0b5b7320c863a5d69b3a45ce9455403a
Shivam Pandey <shivam.pandey@squarepixelstudios.net>
2022-04-15 19:32:10 +0530

    responsive navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicePageComponent } from './service-page/service-page.component';
+import { NewsComponent } from './news/news.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'service', component: ServicePageComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
